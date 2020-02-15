5340b03648fead4e5502127878d5ae537ea5ef28
Matt Martin <matt@enigma.local>
2020-02-15 11:26:00 -0600

    renamed foled and added different defaults

--- code/cli.js
+++ code/cli.js
@@ -1,22 +1,23 @@
 // Write code here to parse command line arguments and store them into variables
 // Add code to print whether the user is searching for an actor or tv show, along with the name of the actor or tv show they are searching for
 
-// Grab search command line argument
 var search = process.argv[2];
-// Joining the remaining arguments since an actor or tv show name may contain spaces
 var term = process.argv.slice(3).join(" ");
 
+// Default search is for a show, "The Daily Show"
 if (!search) {
   search = "show";
-}
-
-if (!term) {
   term = "The Daily Show";
+} else if (!term) {
+  if (search === "show") {
+    term = "The Daily Show";
+  } else if (search === "actor") {
+    term = "John Malkovich";
+  }
 }
 
-// Print whether searching for a show or actor, print the term as well
 if (search === "show") {
-  console.log("Searching for TV Show");
+  console.log("Searching for TV Show: " + term);
 } else {
-  console.log("Searching for TV Actor");
+  console.log("Searching for TV Actor: " + term);
 }
